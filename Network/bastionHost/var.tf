82d5260803e90557d44a1471cb70e57f0013ba64
George Markou <82205061+george-markou@users.noreply.github.com>
2023-03-01 00:41:00 +0200

    Update var.tf
    added missing description on variables

--- Network/bastionHost/var.tf
+++ Network/bastionHost/var.tf
@@ -1,49 +1,60 @@
 variable "pip_name" {
   type = string
-  description = "The name of the bastion host public IP."
+  description = "(Required) Specifies the name of the Public IP."
 }
 
 variable "bastion_name" {
   type = string
-  description = "The name of the bastion host."
+  description = "(Required) Specifies the name of the Bastion Host."
 }
 
 variable "location" {
   type = string
-  description = "The name of the location."
+  description = "(Required) Specifies the supported Azure location where the resource exists."
 }
 
 variable "resource_group_name" {
   type = string
-  description = "The resource group name."
+  description = "(Required) The name of the resource group in which to create the Bastion Host."
 }
 
 variable "subnet_id" {
   type = string
-  description = "The id of the AzureBastionSubnet."
+  description = "(Required) The id of the AzureBastionSubnet."
 }
 
 variable "copy_paste_enabled" {
   type = bool
-  description = "Copy & Paste capability."
+  description = "(Optional) Is Copy/Paste feature enabled for the Bastion Host. Defaults to true."
+  default = true
 }
 
 variable "file_copy_enabled" {
   type = bool
-  description = "File copy capability."
+  description = "(Optional) Is File Copy feature enabled for the Bastion Host. Defaults to false."
+  default = null
 }
 
 variable "sku" {
   type = string
-  description = "SKU for the bastion host "
+  description = "(Optional) The SKU of the Bastion Host. Accepted values are Basic and Standard. Defaults to Basic."
+  default = "Basic"
 }
 
 variable "ip_connect_enabled" {
   type = bool
-  description = "IP Connect capability."
+  description = "(Optional) Is IP Connect feature enabled for the Bastion Host. Defaults to false."
+  default = null
 }
 
 variable "shareable_link_enabled" {
   type = bool
-  description = "Shareable link capability."
+  description = "(Optional) Is Shareable Link feature enabled for the Bastion Host. Defaults to false."
+  default = null
+}
+
+variable "tags" {
+  description = "(Optional) A mapping of tags which should be assigned to the Azure Bastion Host and Public IP resources."
+  type = map(string)
+  default = null
 }
