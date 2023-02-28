628ba9155fb8a4c3fe76832fdb662b6da0d0193a
George Markou <82205061+george-markou@users.noreply.github.com>
2023-03-01 00:44:38 +0200

    Update output.tf
    added missing output information

--- Network/bastionHost/output.tf
+++ Network/bastionHost/output.tf
@@ -1,4 +1,24 @@
-output "name" {
+output "bastion_name" {
   description = "The name of the bastion resource"
   value = azurerm_bastion_host.bastion.name
+}
+
+output "bastion_id" {
+  description = "The ID of the Bastion Host."
+  value = azurerm_bastion_host.bastion.name
+}
+
+output "dns_name" {
+  description = "The FQDN for the Bastion Host."
+  value = azurerm_bastion_host.bastion.dns_name
+}
+
+output "pip_id" {
+  description = "The FQDN for the Bastion Host."
+  value = azurerm_public_ip.pip.id
+}
+
+output "pip_address" {
+  description = "The IP address value that was allocated."
+  value = azurerm_public_ip.pip.ip_address
 }
